060c0b29aa139bc8816c68307f2401de9b54f896
Anurag-Kalwa <anurag852001@gmail.com>
2022-04-29 08:50:17 +0530

    commit on'Master'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+import NewExpense from "./components/NewExpense";
+import React, { useState } from "react";
+import ExpensesFilter from "./components/ExpenseFilter";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseChart from "./components/ExpenseChart";
+
+const DummyData = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
+  const [expenses, addExpenses] = useState(DummyData);
+  function onDataReceiveHandler(NewExpenseData) {
+    addExpenses(() => {
+      return [NewExpenseData, ...expenses];
+    });
+  }
+
+  const [filterDate, setFilterDate] = useState("2021");
+  const onFilterChangeHandler = (FDate) => {
+    setFilterDate(FDate);
+  };
+
+  const DataFilter = expenses.filter((expenses) => {
+    return expenses.date.getFullYear().toString() === filterDate;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <NewExpense onDataRecieve={onDataReceiveHandler}></NewExpense>
+        <ExpenseChart items={DataFilter}></ExpenseChart>
+        <ExpensesFilter onFilterChange={onFilterChangeHandler}></ExpensesFilter>
+        <ExpenseList items={DataFilter}></ExpenseList>
+      </div>
     </div>
   );
 }
